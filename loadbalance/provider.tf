c3e47cc5b89639b140da25af452dcfc4e53b4e1f
Alexandru Dumea <a.dumea@modigagency.com>
2022-07-06 12:42:51 +0300

    terraform

---
+++ loadbalance/provider.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Set the variable value in *.tfvars file
+# or using -var="do_token=..." CLI option
+variable "do_token" {}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_app" "digitaloceans-symfony" {
+  spec {
+    name   = "digitaloceans-symfony"
+    region = "ams"
+
+    service {
+      name               = "digitaloceans-symfony"
+      environment_slug   = "php"
+      instance_count     = 1
+      instance_size_slug = "professional-xs"
+
+      github {
+       branch         = "master"
+       deploy_on_push = true
+       repo           = "adumea/digitaloceans-symfony"
+      }
+    }
+  }
+}
