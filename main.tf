086f9d27628d67a103ff1001d8939f0a8c2360e5
Alexandru Dumea <a.dumea@modigagency.com>
2022-07-06 15:01:25 +0300

    Wip

--- main.tf
+++ main.tf
@@ -11,12 +11,16 @@ terraform {
 # or using -var="do_token=..." CLI option
 variable "do_token" {}
 
+# or using -var="do_token=..." CLI option
+variable "ssh_fingerprint" {}
+
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = var.do_token
 }
 
 resource "digitalocean_app" "digitaloceans-symfonys" {
+  ssh_keys = var.ssh_fingerprint
   spec {
     name   = "digitaloceans-symfonys"
     region = "ams"
