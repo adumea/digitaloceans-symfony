a7e600bdde6edb20a386c6cad024685057b7e40e
Alexandru Dumea <a.dumea@modigagency.com>
2022-07-06 16:34:27 +0300

    Wip

--- main.tf
+++ main.tf
@@ -16,16 +16,18 @@ variable "secret_key" {}
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token             = var.do_token
+  spaces_access_id  = var.access_id
+  spaces_secret_key = var.secret_key
 }
 
 
-resource "digitalocean_app" "digitaloceans-symfonys" {
+resource "digitalocean_app" "digitaloceans-symfony" {
   spec {
-    name   = "digitaloceans-symfonys"
+    name   = "digitaloceans-symfony"
     region = "ams"
 
     service {
-      name               = "digitaloceans-symfonys"
+      name               = "digitaloceans-symfony"
       environment_slug   = "php"
       instance_count     = 1
       instance_size_slug = "professional-xs"
--- vars.auto.tfvars
+++ vars.auto.tfvars
@@ -1,1 +1,3 @@
 do_token = "dop_v1_7266209cc11a5d0f65db9ec9acb3607645b5bc95a2b78f38dc226a8c80facb11"
+access_id = "4AQJGPPLQDHQ2B53BMGE"
+secret_key = "F3HFIMYNMDCVMG66CVAW"
